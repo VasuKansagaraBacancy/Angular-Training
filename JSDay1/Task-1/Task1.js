d79b498130c395bf0905ec15d547634f26edcde3
VasuKansagaraBacancy <154701855+vasupatel414@users.noreply.github.com>
2025-04-08 15:09:05 +0530

    Updated

--- JSDay1/Task-1/Task1.js
+++ JSDay1/Task-1/Task1.js
@@ -1,5 +1,3 @@
-//<-------------------Task-1------------------->
-
 function getPositiveNumbers(arr) {
     return arr.filter(num => num > 0);
 }
@@ -13,12 +11,3 @@ const nums = [-5,-4,-3, -2, 0, 1, 2, 3, 4, 5];
 console.log("Positive Numbers:", getPositiveNumbers(nums));
 
 console.log("Squared Evens:", getSquaredEvens(nums));
-
-//<-------------------Task-2------------------->
-
-function getFee(isMember) {
-    return isMember ? '$2.00' : '$10.00';
-}
-
-console.log(getFee(true));  
-console.log(getFee(false)); 
